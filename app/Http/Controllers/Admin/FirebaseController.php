ef8ddd6b6aa6631fd5f27e2550a2bf60e3113d42
Daniel Castellanos <dcastell777@gmail.com>
2020-09-27 15:13:00 -0500

    Cambios 27-09-2020

--- app/Http/Controllers/Admin/FirebaseController.php
+++ app/Http/Controllers/Admin/FirebaseController.php
@@ -11,16 +11,15 @@ class FirebaseController extends Controller
 {
     public function sendAll(Request $request)
     {
+
     	$recipients = User::whereNotNull('device_token')->pluck('device_token')->toArray();
     	//dd($recipients);
 
     	fcm()
 		    ->to($recipients) // $recipients must an array
-		    ->priority('high')
-		    ->timeToLive(0)
 		    ->notification([
 		        'title' => $request->input('title'),
-		        'body' => $request->input('body'),
+		        'body' => $request->input('body')
 		    ])
 		    ->send();
 
